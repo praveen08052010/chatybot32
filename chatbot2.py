2ee202356c4170cbede3f2b6cb5efd29d6307ee0
abbhinav23 <97897309+praveen08052010@users.noreply.github.com>
2022-08-16 22:09:07 +0530

    Add files via upload

---
+++ chatbot2.py
@@ -0,0 +1,8 @@
+from cgi import print_arguments
+
+print("Hello Welcome To Modern ChatBot2 #_@")
+name = input("So wats your name?\n")
+print("Oh wlc " + name)
+add = input("So do u wanna add something?")
+if add == "Yes" or "yup":
+    input("So wats the number?")
